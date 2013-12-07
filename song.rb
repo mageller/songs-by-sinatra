017da1c8bf6bf6362e727b9cd59c6fb4ab53adcb
Mark Geller <mgeller@enmasse.com>
2013-12-06 16:55:09 -0800

    Added CRUD functions for managing songs

--- song.rb
+++ song.rb
@@ -10,6 +10,51 @@ class Song
   property :lyrics, Text
   property :length, Integer
   property :released_on, Date
+
+  def released_on=date
+    super Date.strptime(date, '%m/%d/%Y')
+  end
 end
 
 DataMapper.finalize
+
+# Get a list of all songs
+get '/songs' do
+  @songs = Song.all
+  slim :songs
+end
+
+# Create a new song
+get '/songs/new' do
+  @song = Song.new
+  slim :new_song
+end
+
+post '/songs' do
+  song = Song.create(params[:song])
+  redirect to("/songs/#{song.id}")
+end
+
+# View a particular song
+get '/songs/:id' do
+  @song = Song.get(params[:id])
+  slim :show_song
+end
+
+# Edit a song
+get '/songs/:id/edit' do
+  @song = Song.get(params[:id])
+  slim :edit_song
+end
+
+put '/songs/:id' do
+  song = Song.get(params[:id])
+  song.update(params[:song])
+  redirect to("/songs/#{song.id}")
+end
+
+# Delete an existing song
+delete '/songs/:id' do
+  Song.get(params[:id]).destroy
+  redirect to('/songs')
+end
